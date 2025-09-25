288465b3e2a9927497f86011cf398b4a2ad2b3a5
Moon_Moon_Demadog <weilandlynn2@gmail.com>
2025-09-24 22:27:48 -0400

    Finished tinkering with the code. File still won't run no matter what I've tried and AI wasn't able to help fix it either.

--- Procedural_Programming_Project.cpp
+++ Procedural_Programming_Project.cpp
@@ -5,77 +5,123 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-using namespace std;
+#include <iomanip>
 
-// function prototypes
-void readDataFromFile(const string& filename, int& studentCount);
-string studentNames[MAX_STUDENTS][MAX_GRADES];
-float studentAverages[MAX_STUDENTS] = {0};
+using namespace std;
 const int MAX_STUDENTS = 50;
-const int MAX_GRADES = 5;
+const int TEST_SCORE = 5;
+
+/* 
+getStudentData:
+Preconditions- a file has already been opened and validated The number
+               of records on file is unknown.
+Input- file object
+Postconditions- Student names are in an arrya of strings. Grades are in a
+                2D array of integers with 5 grades per student and the
+				students names are in the same order as their grades.
+				another parralel array contains the average grade for each student.
+				The number of students on file will be returned.
+Output- array of strinsg, 2D array of integers, char and the record count.
+*/
+int getStudentData(string students[], double grades[][TEST_SCORE], int maxRows, ifstream& sData);
+char getLetterGrade(double average);
+/* report:
+* Preconditions- all the data is available (student names, grades, average grades, letter grades)
+* Input- name array, grades array(2D), average grades array, letter grades array, and the number of students
+* Postconditions- a formated report is output to the screen
+*/
+
+void report(const string students[], const double grades[][TEST_SCORE], int numStudents, double avgGrades[], const char letterGrades[]);
+
+/* calculateAVGrade
+* Preconditions- test scores for a student are in an array of integers
+* Input- 2D array of integers, and the number of test scores
+* Postconditions- the average of the test scores is calculated and returned
+* Output- the average of the test scores
+*			as well as an array of letter grades for each student.
+*			and a double array of average grades for each student.
+*/
+void calculateAVGGrade(const double grades[][TEST_SCORE], int numStudents, double avgGrades[]);
 
 int main()
 {
-	// arrays to hold student data;
-	char studentLetterGrades[MAX_STUDENTS] = { 0 };
-	int studentCount = 0;
-	string filename = "StudentGrades.txt";
-
+	string studentNames[MAX_STUDENTS];
+	double studentGrades[MAX_STUDENTS][TEST_SCORE];
+	double avgGrades[MAX_STUDENTS];
+	char letterGrades[MAX_STUDENTS] = { MAX_STUDENTS };
+	int numStudents;
+	ifstream inFile("TextFile1.tx");
+	if (inFile.fail())
+	{
+		cout << "Input file did not open correctly. Program ending." << endl;
+		return 1;
+	}
+	numStudents = getStudentData(studentNames, studentGrades, MAX_STUDENTS, inFile);
+	calculateAVGGrade(studentGrades, numStudents, avgGrades);
+	for (int i = 0; i < numStudents; i++)
+	{
+		letterGrades[i] = getLetterGrade(avgGrades[i]);
+	}
+	report(studentNames, studentGrades, numStudents, avgGrades, letterGrades );
 }
 
-// function to read data from file and get student count
-void readDataFromFile(const string& filename, int& studentCount)
+int getStudentData(string students[], double grades[][TEST_SCORE], int maxRows, ifstream& sData)
 {
-	ifstream inputFile(filename);
-	if (!inputFile)
-	{
-		cout << "Error opening file: " << filename << endl;
-		return;
-	}
-	string name;
-	float grade;
-	while (inputFile >> name)
-	{
-		for (int i = 0; i < 5; ++i)
+		int count = 0;
+		while (count < maxRows && sData >> students[count])
 		{
-			if (inputFile >> grade)
+			for (int j = 0; j < TEST_SCORE; j++)
 			{
-				// Store the name and grades in the arrays
-				studentNames[studentCount][0] = name;
-				studentNames[studentCount][i + 1] = to_string(grade);
+				sData >> grades[count][j];
 			}
-			else
-			{
-				break; // No more grades for this student
-			}
+			count++;
 		}
-		studentCount++;
-	}
-	inputFile.close();
+		sData.close();
+	return count;
 }
 
-// function to calculate average grades
-static void calculateAverages(int studentCount, float studentAverages[])
+void report(const string students[], const double grades[][TEST_SCORE], int numStudents, double avgGrades[], const char letterGrades[])
 {
-	for (int i = 0; i < studentCount; ++i)
+	cout << "Student Grade Report" << endl;
+	cout << "====================" << endl;
+	cout << fixed << setprecision(2);
+	cout << "Student Name     " << "  Grade               " << "Average Grade " << "Letter Grade" << endl;
+	cout << "========================================================================================" << endl;
+	for (int i = 0; i < numStudents; i++)
 	{
-		float sum = 0;
-		int gradeCount = 0;
-		for (int j = 1; j <= MAX_GRADES; ++j)
+		cout << left << setw(10) << students[i] << right;
+		for (int j = 0; j < TEST_SCORE; j++)
 		{
-			if (!studentNames[i][j].empty())
-			{
-				sum += stof(studentNames[i][j]);
-				gradeCount++;
-			}
+			cout << setw(14) << grades[i][j];
 		}
-		if (gradeCount > 0)
-		{
-			studentAverages[i] = sum / gradeCount;
-		}
-		else
-		{
-			studentAverages[i] = 0;
-		}
+		cout << setw(14) << avgGrades[i] << setw(14) << letterGrades[i] << endl;
 	}
 }
+
+void calculateAVGGrade(const double grades[][TEST_SCORE], int numStudents, double avgGrades[])
+{
+	for (int i = 0; i < numStudents; i++)
+	{
+		double total = 0.0;
+		for (int j = 0; j < TEST_SCORE; j++)
+		{
+			total += grades[i][j];
+		}
+		avgGrades[i] = total / TEST_SCORE;
+	}
+}
+
+char getLetterGrade(double average)
+{
+	if (average >= 90.0)
+		return 'A';
+	else if (average >= 80.0)
+		return 'B';
+	else if (average >= 70.0)
+		return 'C';
+	else if (average >= 60.0)
+		return 'D';
+	else
+		return 'F';
+}
+// End of program
